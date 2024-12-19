c5dfbe2d17526f18ee710c429c5cd2e56cddac47
PetoriousBIG <peter.m.martin.1993@gmail.com>
2024-12-18 23:20:04 -0500

    temp

--- Regression/utils/general.py
+++ Regression/utils/general.py
@@ -20,3 +20,25 @@ def split_data(df, target_variable, test_ratio, split_random_state):
         train_test_split(data, target, test_size=test_ratio, random_state=split_random_state, shuffle=True, stratify=None)
 
     return training_data, testing_data, training_target, testing_target
+
+def calculate_missingness(df, threshold):
+    missingness_drop_list = []
+    missingness_vals = df.isna().sum() / df.shape[0]
+    print(missingness_vals)
+    for attribute in df.columns:
+        if missingness_vals[attribute] >= threshold:
+            missingness_drop_list.append(attribute)
+    return missingness_drop_list
+
+def print_uniqueness(df):
+    print(f'The data frame has {df.shape[0]} rows\n')
+
+    concern_list = []
+    for attribute in df.columns:
+        label = ''
+        if df[attribute].nunique() == df.shape[0]:
+            label = 'examine more closely'
+            concern_list.append(attribute)
+        print(f'{attribute} has {df[attribute].nunique()} unique values and is dtype {df[attribute].dtype} {label}')
+
+    return concern_list
